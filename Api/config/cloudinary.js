d4046ae03df79815c6408da8bf5761e4031e0097
gokul <gokulchandan24@gmail.com>
2024-10-11 01:11:02 +0530

    backend done

--- Api/config/cloudinary.js
+++ Api/config/cloudinary.js
@@ -4,9 +4,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 cloudinary.config({
-	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-	api_key: process.env.CLOUDINARY_API_KEY,
-	api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: "dixptruyr",
+  api_key: "532696811226456",
+  api_secret: "TgSEegJweisxQsif3roPkpspzXk",
 });
 
 export default cloudinary;
